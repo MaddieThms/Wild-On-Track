2157bc5b4daeba52d379ab3940406e9b7175ede4
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-10-24 14:57:09 +0200

    Header operationnal

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,154 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Bookmarks, Call, Flight, Home } from "@mui/icons-material";
+import { ListItemIcon } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import logoImg from "../assets/logo.png";
+
+const drawerWidth = 240;
+
+const navigateItems = [
+  {
+    index: 0,
+    icon: <Home />,
+    label: "Accueil",
+    route: "accueil",
+  },
+  {
+    index: 1,
+    icon: <Flight />,
+    label: "Nouveau voyage",
+    route: "nouveau-voyage",
+  },
+  {
+    index: 2,
+    icon: <Bookmarks />,
+    label: "Mes Carnets",
+    route: "mes-carnets",
+  },
+  {
+    index: 3,
+    icon: <Call />,
+    label: "Contact",
+    route: "contact",
+  },
+];
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
+
+export default function PersistentDrawerRight() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar sx={{ bgcolor: "#eee" }}>
+          <Typography noWrap sx={{ flexGrow: 1 }} mt={1} component="div">
+            <img src={logoImg} alt="logo" width={60} />
+          </Typography>
+
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+            size="large"
+          >
+            <MenuIcon sx={{ color: "black" }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          bgcolor: "#eaa226",
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader sx={{ bgcolor: "#eaa226" }}>
+          <IconButton onClick={handleDrawerClose} sx={{ color: "white" }}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <List sx={{ bgcolor: "#eaa226", height: "100vh", color: "white" }}>
+          {navigateItems.map((item) => (
+            <ListItem
+              button
+              onClick={() => navigate(item.route)}
+              key={item.id}
+              disablePadding
+            >
+              <ListItemButton sx={{ "&:hover": { backgroundColor: "#eee" } }}>
+                <ListItemIcon sx={{ color: "#FCFCFC" }}>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
