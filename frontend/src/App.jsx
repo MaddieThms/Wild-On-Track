ba4cb297bff1707c92a707309c4a510d64ad6136
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-10-25 10:03:42 +0200

    created homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import Home from "./pages/Home";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Home />
     </div>
   );
 }
