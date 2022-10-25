e01aa1951329bffe75d07341d19e5a6feeb8895f
Jonathan Garonian <j.garonian@gmail.com>
2022-10-25 10:19:04 +0200

    problem footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import imgfooter from "../assets/imgfooter.png";
 import "./footer.css";
 
 function Footer() {
-  return (
-    <footer>
-      <img src={imgfooter} alt="imagefooter" />
-    </footer>
-  );
+  return <footer />;
 }
 
 export default Footer;
