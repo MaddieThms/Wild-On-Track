437810cfa96a4653d1ca56a4f1c6b158e607ce20
Jonathan Garonian <j.garonian@gmail.com>
2022-10-25 09:57:42 +0200

    footer finished

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import imgfooter from "../assets/imgfooter.png";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <img src={imgfooter} alt="imagefooter" />
+    </footer>
+  );
+}
+
+export default Footer;
