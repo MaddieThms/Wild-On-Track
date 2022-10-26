f31a88cf7d610d8de2d2677d7c7209c72ec57d08
Jonathan Garonian <j.garonian@gmail.com>
2022-10-26 12:26:57 +0200

    Hotelspage finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-
 import Home from "./pages/Home";
 import MesCarnets from "./pages/MesCarnets";
 import "./App.css";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import FlightPage from "./pages/FlightPage";
+import MesHotels from "./pages/MesHotels";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Home />
       <MesCarnets />
       <FlightPage />
+      <MesHotels />
       <Footer />
     </div>
   );
--- frontend/src/components/PageTitle.jsx
+++ frontend/src/components/PageTitle.jsx
@@ -4,7 +4,7 @@ function PageTitle() {
   return (
     <div style={{ marginTop: "130px", marginBottom: "5px" }}>
       <h1 style={{ textAlign: "center", color: "#40A798" }}>
-        VOLS POUR <span style={{ color: "#eaa226" }}>BALI</span>
+        XXXX POUR <span style={{ color: "#eaa226" }}>BALI</span>
       </h1>
     </div>
   );
