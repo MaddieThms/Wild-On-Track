728b3f327738fb716d84e82f1e24228d0e195aa1
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-11-17 10:41:57 +0100

    loader ok

--- frontend/src/components/cardHotel/Hotels.jsx
+++ frontend/src/components/cardHotel/Hotels.jsx
@@ -12,7 +12,7 @@ function Hotels({ dataHotels }) {
   useEffect(() => {
     setTimeout(() => {
       setLoader(false);
-    }, 3000);
+    }, 5000);
   }, []);
 
   return loader ? (
--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/self-closing-comp */
 import React from "react";
 import "./loader.css";
+import loaderimg from "./loading-plane.gif";
 
 export default function Loader() {
   return (
     <div className="loader fade-in-image">
-      <img
-        src="./src/components/loader/loading-plane.gif"
-        alt=""
-        width="100px"
-        height="100px"
-      ></img>
+      <img src={loaderimg} alt="Loader plane" />
     </div>
   );
 }
