ce3aa155ad046a01d07118134405705d1569ed25
Madeline Thomas <maddie.thms@gmail.com>
2022-11-17 12:08:06 +0100

    added team members portrait

--- frontend/src/components/formulaire/Team.jsx
+++ frontend/src/components/formulaire/Team.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Formulaire.css";
+import teamRyan from "./teamRyan.png";
+import teamMadeline from "./teamMadeline.png";
+import teamIris from "./teamIris.png";
+import teamArnaud from "./teamArnaud.png";
+import teamJohn from "./teamJohn.png";
 
 function Team() {
   return (
     <div className="container-circle">
-      <div className="circle" />
-      <div className="circle" />
-      <div className="circle" />
-      <div className="circle" />
-      <div className="circle" />
+      <img className="circle" src={teamJohn} alt="John" />
+
+      <img className="circle" src={teamIris} alt="Iris" />
+
+      <img className="circle" src={teamArnaud} alt="Arnaud" />
+      <img className="circle" src={teamRyan} alt="Ryan" />
+      <img className="circle" src={teamMadeline} alt="Madeline" />
     </div>
   );
 }
